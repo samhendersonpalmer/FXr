d6c250643cdc32abdeabe65d98c3f6d0d236951a
Sam Henderson-Palmer <samhendersonpalmer@gmail.com>
2025-02-05 12:28:27 +0000

    Added previous 3 day EUR rates function including the 24 hour percentage change over each of the previous 3 days

--- exchange_functions.R
+++ exchange_functions.R
@@ -32,3 +32,36 @@ EUR_rate_today <- function(){
   return(rate_value)
   
 }
+
+
+# Retrieve EUR rate past 3 days -------------------------------------------
+EUR_rate_prev3 <- function(){
+  
+  # Define API base URL
+  base_url_fx <- "https://api.frankfurter.dev/v1/"
+  
+  # Define API endpoints
+  endpoint_fx <- paste0(Sys.Date()-3,"..", Sys.Date(), "?base=GBP&symbols=EUR")
+  
+  # Define request
+  req_fx <- request(base_url_fx) %>% 
+    req_url_path_append(endpoint_fx)
+  
+  # Perform response
+  req_fx_perform <- req_fx |> 
+    req_perform()
+  
+  # Extract results from the response
+  results_fx <- req_fx_perform |> 
+    resp_body_json()
+  
+  # Extract rate
+  rate_values <- stack(results_fx$rates) |> 
+    select(date = ind, rate = values) |> 
+    arrange(desc(date)) |> 
+    mutate(change_24hr = (rate - lead(rate))/lead(rate)*100)
+  
+  return(rate_value)
+  
+}
+
